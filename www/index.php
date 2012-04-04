273d519a58fa986bd05df6ed9e87291c821939c7
Thomas VIAL <github@ifusio.com>
2012-04-04 12:14:30 +0200

    Comments and code indentation + link to external CSS file.

--- www/index.php
+++ www/index.php
@@ -3,23 +3,7 @@
   <head>
     <meta http-equiv="Refresh" content="1">
     <title>Varnish ESI Example</title>
-    <style type="text/css" media="screen">
-      #layout {
-        width:800px;
-        margin:auto;
-      }
-      #left {
-        width:600px;
-        float:left;
-      }
-      #right {
-        margin-left:620px;
-      }
-      #footer {
-        clear:both;
-        text-align: center;
-      }
-    </style>
+    <link rel="stylesheet" href="/css/screen.css" type="text/css" media="screen" charset="utf-8">
   </head>
   <body>
     <div id="layout">
@@ -27,7 +11,9 @@
         <h1>Varnish ESI Example</h1>
       </div>
       <div id="left">
+        <!-- ESI tag -->
         <esi:include src="lorem.php" />
+        <!-- / ESI tag -->
       </div>
       <div id="right">
         <h3>No cache !</h3>
